c058fd1d9b1315ea1f4b99d037d7cb39e26d6474
Raquel Rodriguez <srtademaciel@gmail.com>
2021-05-11 10:25:19 +0200

    config mongoose

--- app.js
+++ app.js
@@ -4,25 +4,16 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
-
-mongoose
-  .connect('mongodb://localhost/applant-backend', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+//MONGOOSE --------------
+require('./configs/mongoose')
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -51,8 +42,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+//ROUTES----------
+app.use('/', require('./routes/index'));
 
 
 module.exports = app;
