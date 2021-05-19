472de69370d3b2f67b3e2c5a88280f02504a6699
Hector <Thornnk1@gmail.com>
2021-05-19 09:37:42 +0200

    Ready to deploy to Heroku

---
+++ public/precache-manifest.18aa9d3482373d4492457992b0b44333.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea22027d646984ef9d2e",
+    "url": "/static/css/main.032d5559.chunk.css"
+  },
+  {
+    "revision": "ea22027d646984ef9d2e",
+    "url": "/static/js/main.ae9e5fa1.chunk.js"
+  },
+  {
+    "revision": "b256109185c6da345f1c",
+    "url": "/static/js/runtime~main.2d047f2f.js"
+  },
+  {
+    "revision": "5e0182837e9e9cc87a8f",
+    "url": "/static/css/2.0d6caef9.chunk.css"
+  },
+  {
+    "revision": "5e0182837e9e9cc87a8f",
+    "url": "/static/js/2.e09a2981.chunk.js"
+  },
+  {
+    "revision": "5bca50d6edd3008f6e71",
+    "url": "/static/css/3.a60c4913.chunk.css"
+  },
+  {
+    "revision": "5bca50d6edd3008f6e71",
+    "url": "/static/js/3.c689b3c8.chunk.js"
+  },
+  {
+    "revision": "ea159c99308be1df1ec3",
+    "url": "/static/js/4.82f6056e.chunk.js"
+  },
+  {
+    "revision": "d967e2ebc9ecd4bdeaf95ec13bed70dc",
+    "url": "/index.html"
+  }
+];
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const User = require('../models/User.model')
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.send('Backend working :)')
 })
