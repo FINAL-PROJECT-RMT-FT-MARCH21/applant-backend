7d03a24d5329f8c9b0b40c4f1a27a7619269de64
Hector <Thornnk1@gmail.com>
2021-05-21 12:27:00 +0200

    Maybe last commit before the storm

---
+++ public/precache-manifest.9574299ce313ffb12a01a4b6b881da8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "511adef32a4e8aa3a291",
+    "url": "/static/css/main.809bab5c.chunk.css"
+  },
+  {
+    "revision": "511adef32a4e8aa3a291",
+    "url": "/static/js/main.f1ab1848.chunk.js"
+  },
+  {
+    "revision": "cb2c877f614d68c82f10",
+    "url": "/static/js/runtime~main.3a2c9e5a.js"
+  },
+  {
+    "revision": "4ee3047311a471c7b5e9",
+    "url": "/static/css/2.0d6caef9.chunk.css"
+  },
+  {
+    "revision": "4ee3047311a471c7b5e9",
+    "url": "/static/js/2.6d7895b6.chunk.js"
+  },
+  {
+    "revision": "40bf10075b4762cdeb7b",
+    "url": "/static/css/3.5084a3e4.chunk.css"
+  },
+  {
+    "revision": "40bf10075b4762cdeb7b",
+    "url": "/static/js/3.ecfc3484.chunk.js"
+  },
+  {
+    "revision": "0d3a11b696d933ef88cf",
+    "url": "/static/js/4.b506b26c.chunk.js"
+  },
+  {
+    "revision": "3ff3069bba5d2768fc83230529044073",
+    "url": "/index.html"
+  }
+];
